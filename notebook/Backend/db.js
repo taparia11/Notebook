578663b7383e27fef7f41b49a69186faaad23191
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-22 21:47:25 +0530

    Mongoose setup completed

---
+++ notebook/Backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("connected to Mongo")
+    })
+}
+
+module.exports = connectToMongo;
