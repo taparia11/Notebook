94c39b117dfd8ca3ae6b2b991373e384a8c1cb21
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-23 21:25:49 +0530

    router used

--- notebook/Backend/index.js
+++ notebook/Backend/index.js
@@ -5,9 +5,14 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// Available routes 
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
