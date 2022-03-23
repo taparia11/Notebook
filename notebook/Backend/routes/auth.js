13982d638d7900c6d506dddd5c0add74f9398ecf
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-24 00:17:56 +0530

    validator added and some comments added

--- notebook/Backend/routes/auth.js
+++ notebook/Backend/routes/auth.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult} = require('express-validator');
+const { request } = require('express');
+const { exists } = require('../models/User');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+// Create a user using POST "/api/auth/createuser" No Login required
+router.post('/createuser',[
+    body('name', 'Enter a valid Name').isLength({min: 3}),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Enter a valid Password').isLength({min: 3}),
+
+] , async (req, res)=>{
+    // if there are error return bad request and error
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+//  check whether the user with this email exists already
+   let user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error: 'This Email is already used', message: err.message})})
+    
+
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save()
+    // res.send(req.body)
 })
 
 module.exports = router
