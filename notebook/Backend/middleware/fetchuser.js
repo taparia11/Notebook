d78709b080cbb56742cca008d8a0032a58e94fcf
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-24 22:07:22 +0530

    fetch user details from db using auth-token

---
+++ notebook/Backend/middleware/fetchuser.js
@@ -0,0 +1,19 @@
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'nikhilji';
+
+const fetchuser = (req,res,next)=>{
+    // Get the user from the jwt token and add id to req object
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({error: "Please authenticate using a valid token"})
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({error: "Don't Be Over Smart We started Tracing You 😊"})
+    }
+    
+}
+module.exports = fetchuser;
