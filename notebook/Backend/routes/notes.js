61072d5513d8421db6a43833c76e76f372ab39c4
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-24 22:09:20 +0530

    fetch all notes from DB

--- notebook/Backend/routes/notes.js
+++ notebook/Backend/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const fetchuser = require('../middleware/fetchuser');
 const router = express.Router();
+const Notes = require('../models/Notes');
 
-router.get('/', (req, res)=>{
-    res.json([])
+// ROUTE 1: Get all the notes using GET "/api/auth/fetchallnotes" Login required
+router.get('/fetchallnotes',fetchuser, async (req, res)=>{
+    const notes = await Notes.find({user: req.user.id});
+    res.json(notes) 
 })
 
 module.exports = router
