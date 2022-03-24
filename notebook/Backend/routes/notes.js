c8481b5bd60e176d304981ed8d015cf697b4c014
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-25 01:03:30 +0530

    file name changed and update notes and creat not and fetch not working fine till this commit

--- notebook/Backend/routes/notes.js
+++ notebook/Backend/routes/notes.js
@@ -1,12 +1,68 @@
 const express = require('express');
 const fetchuser = require('../middleware/fetchuser');
 const router = express.Router();
-const Notes = require('../models/Notes');
+const Note = require('../models/Note');
+const { body, validationResult} = require('express-validator');
 
-// ROUTE 1: Get all the notes using GET "/api/auth/fetchallnotes" Login required
+// ROUTE 1: Get all the notes using GET "/api/notes/fetchallnotes" Login required
 router.get('/fetchallnotes',fetchuser, async (req, res)=>{
-    const notes = await Notes.find({user: req.user.id});
-    res.json(notes) 
+    try {
+        const notes = await Note.find({user: req.user.id});
+        res.json(notes)    
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+// ROUTE 2: Add a New Note using POST "/api/notes/addnote" Login required
+router.post('/addnote',[
+    body('title', 'Enter a valid Title').isLength({min: 1}),
+    body('description', 'Description must be 5 characters').isLength({min: 5}),
+],fetchuser, async (req, res)=>{
+
+    try {
+    
+    const{title, description, tag} = req.body;
+    // if there are error return bad request and error
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const note = new Note({
+        title, description, tag, user: req.user.id
+    })
+
+    const saveNote = await note.save()
+
+    res.json(saveNote) 
+        
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+}
+})
+
+// ROUTE 3: Update existing Note using PUT "/api/notes/updatenote" Login required
+router.put('/updatenote/:id',fetchuser, async (req, res)=>{
+    const {title, description, tag} = req.body;
+    // create a new note object
+    const newNote = {};
+    if (title) {newNote.title = title};
+    if (description) {newNote.description = description};
+    if (tag) {newNote.tag = tag};
+
+    // find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(400).send("Not Found")}
+
+    if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
 })
 
 module.exports = router
