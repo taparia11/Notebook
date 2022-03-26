62292ec98964de599328f5fc54336936c34d183a
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-03-26 11:02:36 +0530

    Roouter added

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About'
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is my NoteBook</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/about" element={<About />}/>
+          </Routes>
+    </Router>
+    </>
   );
 }
 
