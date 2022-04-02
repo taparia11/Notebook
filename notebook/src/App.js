f49bd0ca24e2e74136c97e9cec6254d4053f2ead
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-02 21:50:02 +0530

    Mistake of other project resolved

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -1,29 +1,25 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 import Navbar from './components/Navbar';
-// import Home from './components/Home';
-// import About from './components/About'
-import TextForm from "./components/TextForm";
+import Home from './components/Home';
+import About from './components/About'
+
 
 function App() {
   return (
     <>
       <Navbar heading='Gamer' />
-      <div className="container my-3">
-      <TextForm/>
-
-      </div>
-    {/* <Router> */}
-      {/* <Routes> */}
-          {/* <Route exact path="/" element={<Home />}/> */}
-          {/* <Route exact path="/about" element={<About />}/> */}
+    <Router>
+      <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/about" element={<About />}/>
           {/* <Route exact path="/login" element={<Login />}/> */}
-          {/* </Routes>
-    </Router> */}
+          </Routes>
+    </Router>
     </>
   );
 }
