c5b4bbcffd6599a89333da0363b1be3668ac6bc0
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-02 21:30:57 +0530

    updated

--- notebook/src/components/Navbar.js
+++ notebook/src/components/Navbar.js
@@ -3,7 +3,7 @@ import {  Link } from "react-router-dom";
 
 
 const Navbar = () => {
-  return (
+  return ( 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">Title Here</Link>
