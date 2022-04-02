85814b808ee3528b89dadceb3b57b202c3d70bee
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-02 21:30:34 +0530

    Modifyed

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -5,8 +5,9 @@ import {
 } from "react-router-dom";
 
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About'
+// import Home from './components/Home';
+// import About from './components/About'
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
@@ -14,9 +15,10 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route exact path="/about" element={<About />}/>
+          {/* <Route exact path="/" element={<Home />}/> */}
+          {/* <Route exact path="/about" element={<About />}/> */}
           {/* <Route exact path="/login" element={<Login />}/> */}
+          <TextForm/>
           </Routes>
     </Router>
     </>
