ed921c2a40b3332981d58b66b35a10bb8c4eb9f7
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-02 21:31:03 +0530

    added

---
+++ notebook/src/components/TextForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Textform() {
+  return (
+    <div>
+        <div ClassName="mb-3">
+            <label for="myBox" ClassName="form-label">Example textarea</label>
+            <textarea ClassName="form-control" id="myBox" rows="3"></textarea>
+        </div>
+    </div>
+  )
+}
