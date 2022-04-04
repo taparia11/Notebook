b4c702ec8ed16c031d9514c9fb19351a6a201b67
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-04 23:18:02 +0530

    updated

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -7,19 +7,22 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About'
+import NoteState from "./components/context/notes/NoteState";
 
 
 function App() {
   return (
     <>
+    <NoteState>
       <Navbar heading='Gamer' />
     <Router>
       <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="about" element={<About/>}/>
           {/* <Route exact path="/login" element={<Login />}/> */}
           </Routes>
     </Router>
+    </NoteState>
     </>
   );
 }
