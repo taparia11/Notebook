e31d82a9b0ed3ce80eca3a1e9f854602fe2c5ae4
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:11:14 +0530

    fixes

--- notebook/src/components/Noteitem.js
+++ notebook/src/components/Noteitem.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Noteitem = (props) => {
     const { note } = props;
     return (
         <div className='col-md-3'>
-            <div class="card" >
-                    <div class="card-body my-3">
-                        <h5 class="card-title">{note.title}</h5>
-                        <p class="card-text">{note.description}</p>
-                        <a href="#" class="btn btn-primary">Go somewhere </a>
-                        <a href="#" class="btn btn-danger">Go somewhere</a>
+            <div className="card" >
+                    <div className="card-body my-3">
+                        <h5 className="card-title">{note.title}</h5>
+                        <p className="card-text">{note.description}</p>
+                        <Link to="#" className="btn btn-primary mx-2">Go somewhere </Link>
+                        <Link to="#" className="btn btn-danger">Go somewhere</Link>
                     </div>
             </div>
         </div>
