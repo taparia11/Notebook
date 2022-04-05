dc52ca872e101ce620dddb84b1af8ab0124d16a7
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:11:27 +0530

    add note and some functions

--- notebook/src/components/Notes.js
+++ notebook/src/components/Notes.js
@@ -1,15 +1,19 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
+import Addnote from "./Addnote";
 
 export const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, setNotes } = context;
+  const { notes } = context;
   return (
-    <div className="row my-3">
-      {notes.map((note) => {
-        return <Noteitem note={note}/>
-      })}
-    </div>
+    <>
+      <Addnote />
+      <div className="row my-3">
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
   );
 };
