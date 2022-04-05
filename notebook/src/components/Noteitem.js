9e1aa4cae911423e47cbf6f7d6771dbc5f6fb4d2
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:49:03 +0530

    delete button added

--- notebook/src/components/Noteitem.js
+++ notebook/src/components/Noteitem.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import { Link } from 'react-router-dom';
+import noteContext from "../context/notes/noteContext";
 
 
 const Noteitem = (props) => {
     const { note } = props;
+    const context = useContext(noteContext);
+  const { deleteNote } = context;
     return (
         <div className='col-md-3'>
             <div className="card" >
                     <div className="card-body my-3">
                         <h5 className="card-title">{note.title}</h5>
                         <p className="card-text">{note.description}</p>
-                        <Link to="#" className="btn btn-primary mx-2">Go somewhere </Link>
-                        <Link to="#" className="btn btn-danger">Go somewhere</Link>
+                        <Link to="#" className="btn btn-danger mx-2" onClick={()=>{deleteNote(note._id)}}>Delete </Link>
+                        {/* <Link to="#" className="btn btn-danger">Go somewhere</Link> */}
                     </div>
             </div>
         </div>
