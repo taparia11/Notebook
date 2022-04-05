0a831406e6ec9fcd9a726da3844a9c16276e5458
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:12:32 +0530

    functions created of add edit delete

--- notebook/src/context/notes/NoteState.js
+++ notebook/src/context/notes/NoteState.js
@@ -5,7 +5,7 @@ const NoteState = (props) =>{
 
     const notesInitial = [
         {
-            "_id": "623cc5b408156a2b1b9129a8",
+            "_id": "623cc5b408156a2db1b9129a8",
             "user": "623bbd05d0b9b6d5404fb94e",
             "title": "My Note",
             "description": "This is my First Note.",
@@ -14,7 +14,7 @@ const NoteState = (props) =>{
             "__v": 0
           },
           {
-            "_id": "623cc5dd08156a2b1b9129aa",
+            "_id": "623cc5dd08156dda2b1b9129aa",
             "user": "623bbd05d0b9b6d5404fb94e",
             "title": "BOOKED GAS",
             "description": "Your GAS Booking has been confirmed.",
@@ -24,10 +24,36 @@ const NoteState = (props) =>{
           }
     ]
 
+    
+
+    //add a Note
+    const addNote = (title, description, tag)=>{
+        console.log("adding a new Note")
+        const note = {
+            "_id": "623cc5dd08156a2b1bs9129aa",
+            "user": "623bbd05d0b9b6d5404fb94e",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2022-03-24T19:26:21.448Z",
+            "__v": 0
+          };
+        setNotes(notes.concat(note))
+    }
+    //Delete a note
+    const deleteNote = ()=>{
+        
+    }
+
+    //edit a note
+    const editNote = ()=>{
+        
+    }
+
     const [notes, setNotes] = useState(notesInitial)
 
     return(
-        <NoteContext.Provider value={{notes , setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
