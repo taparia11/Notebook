f2621d997c3b9ea04757d63f762cbe7f551b8f80
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:11:05 +0530

    minor upgrades

--- notebook/src/components/Navbar.js
+++ notebook/src/components/Navbar.js
@@ -1,32 +1,33 @@
-import React, {useEffect} from 'react'
+import React from 'react'
+// import {useEffect} from 'react'
 import {  Link, useLocation } from "react-router-dom";
 // import PropTypes from 'prop-types'
 
 
 const Navbar = (props) => {
   let location = useLocation();
-  useEffect(()=>{
-    console.log(location.pathname);
-  },[location]);
+  // useEffect(()=>{
+  //   console.log(location.pathname);
+  // },[location]);
   return ( 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">{props.title}</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <Link class={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</Link>
         </li>
-        <li class="nav-item">
-          <Link class={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">About</Link>
+        <li className="nav-item">
+          <Link className={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">About</Link>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
