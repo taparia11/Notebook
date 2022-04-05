6dac4cede4c4497370b45d14a2d3ad33f26b7d9e
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-05 23:10:44 +0530

    add note working

---
+++ notebook/src/components/Addnote.js
@@ -0,0 +1,57 @@
+import React, { useContext, useState } from "react";
+import noteContext from "../context/notes/noteContext";
+
+const Addnote = () => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({title:"", description:"",tag:"default"})
+  const handleClick = (e)=>{
+      e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+  }
+  const onChange = (e)=>{
+    setNote({...note, [e.target.name]: e.target.value })
+}
+  return (
+    <div className='container'>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="title"
+            name="title"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="description"
+            name="description"
+            onChange={onChange}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Add Note
+        </button>
+      </form>n
+    </div>
+  );
+};
+
+export default Addnote;
