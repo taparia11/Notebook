4871c9bea8ba1815b981eced54aefdc2ca6da20b
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 23:48:06 +0530

    updated

--- notebook/src/components/Noteitem.js
+++ notebook/src/components/Noteitem.js
@@ -9,14 +9,18 @@ const Noteitem = (props) => {
   const { deleteNote } = context;
     return (
         <div className='col-md-3'>
-            <div className="card" >
+            <div className="card text-center" >
+            <h5 className="card-header">{note.tag}</h5>
                     <div className="card-body my-3">
                         <h5 className="card-title">{note.title}</h5>
                         <p className="card-text">{note.description}</p>
                         <Link to="#" className="btn btn-danger mx-2" onClick={()=>{deleteNote(note._id)}}>Delete Note </Link>
-                        <Link to="#" className="btn btn-primary" onClick={()=>{updateNote(note)}}>Update Note</Link>
+                        <Link to="#" className="btn btn-success" onClick={()=>{updateNote(note)}}>Update Note</Link>
                     </div>
             </div>
+            <div className="card-footer text-muted text-center">
+    {note.date.substring(0,10)}
+  </div>
         </div>
     )
 }
