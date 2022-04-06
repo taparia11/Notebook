5cc6c16a9872854cc0a3e1f259c8f92344ef22c5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 19:31:17 +0530

    major update feature added

--- notebook/src/components/Addnote.js
+++ notebook/src/components/Addnote.js
@@ -13,52 +13,52 @@ const Addnote = () => {
     setNote({...note, [e.target.name]: e.target.value })
 }
   return (
-    <div className='container'>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="title" className="form-label">
-            Title
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="title"
-            name="title"
-            aria-describedby="emailHelp"
-            onChange={onChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="description"
-            name="description"
-            onChange={onChange}
-          />
-        </div>
+      <div className='container'>
+          <form>
+              <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                      Title
+                  </label>
+                  <input
+                      type="email"
+                      className="form-control"
+                      id="title"
+                      name="title"
+                      aria-describedby="emailHelp"
+                      onChange={onChange}
+                  />
+              </div>
+              <div className="mb-3">
+                  <label htmlFor="description" className="form-label">
+                      Description
+                  </label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      id="description"
+                      name="description"
+                      onChange={onChange}
+                  />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="tag" className="form-label">
-            Tag
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="tag"
-            name="tag"
-            onChange={onChange}
-          />
-        </div>
-    
-        <button type="submit" className="btn btn-primary" onClick={handleClick}>
-          Add Note
-        </button>
-      </form>
-    </div>
+              <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">
+                      Tag
+                  </label>
+                  <input
+                      type="text"
+                      className="form-control"
+                      id="tag"
+                      name="tag"
+                      onChange={onChange}
+                  />
+              </div>
+
+              <button type="submit" className="btn btn-primary" onClick={handleClick}>
+                  Add Note
+              </button>
+          </form>
+      </div>
   );
 };
 
