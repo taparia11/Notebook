7358f8002073520796bffae23be01760e5c5f365
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 23:47:14 +0530

    updated

--- notebook/src/components/Addnote.js
+++ notebook/src/components/Addnote.js
@@ -8,55 +8,28 @@ const Addnote = () => {
   const handleClick = (e)=>{
       e.preventDefault();
     addNote(note.title, note.description, note.tag);
+    setNote({title:"", description:"",tag:""})
   }
   const onChange = (e)=>{
     setNote({...note, [e.target.name]: e.target.value })
 }
+
   return (
       <div className='container'>
           <form>
               <div className="mb-3">
-                  <label htmlFor="title" className="form-label">
-                      Title
-                  </label>
-                  <input
-                      type="email"
-                      className="form-control"
-                      id="title"
-                      name="title"
-                      aria-describedby="emailHelp"
-                      onChange={onChange}
-                  />
+                  <label htmlFor="title" className="form-label"> Title </label>
+                  <input type="email" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange} minLength={5} required />
               </div>
               <div className="mb-3">
-                  <label htmlFor="description" className="form-label">
-                      Description
-                  </label>
-                  <input
-                      type="text"
-                      className="form-control"
-                      id="description"
-                      name="description"
-                      onChange={onChange}
-                  />
+                  <label htmlFor="description" className="form-label"> Description </label>
+                  <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} minLength={5} required />
               </div>
-
               <div className="mb-3">
-                  <label htmlFor="tag" className="form-label">
-                      Tag
-                  </label>
-                  <input
-                      type="text"
-                      className="form-control"
-                      id="tag"
-                      name="tag"
-                      onChange={onChange}
-                  />
+                  <label htmlFor="tag" className="form-label"> Tag </label>
+                  <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} minLength={5} required />
               </div>
-
-              <button type="submit" className="btn btn-primary" onClick={handleClick}>
-                  Add Note
-              </button>
+              <button disabled={note.title.length<5 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}> Add Note </button>
           </form>
       </div>
   );
