8450effd40f82882ac1c9a4bf73eb977566e4296
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 23:47:57 +0530

    buttons added login/signup

--- notebook/src/components/Navbar.js
+++ notebook/src/components/Navbar.js
@@ -25,10 +25,12 @@ const Navbar = (props) => {
           <Link className={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">About</Link>
         </li>
       </ul>
-      <form className="d-flex">
+      <form className="d-flex mx-2">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+      <Link className="btn btn-danger " to="/signup" role="button">SignUp</Link>
     </div>
   </div>
 </nav>
