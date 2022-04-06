124ab44269e6423a19a4ba56cd37689eddfb0c67
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 23:46:38 +0530

    new components added

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -8,6 +8,8 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About'
 import NoteState from "./context/notes/NoteState";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/about" element={<About/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/signup" element={<Signup/>} />
           {/* <Route exact path="/login" element={<Login />}/> */}
           </Routes>
     </Router>
