396be877bd2ca59bf3d94a8507ea3f14cbcfcfd8
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-06 19:31:00 +0530

    updated

--- notebook/src/components/Addnote.js
+++ notebook/src/components/Addnote.js
@@ -4,7 +4,7 @@ import noteContext from "../context/notes/noteContext";
 const Addnote = () => {
   const context = useContext(noteContext);
   const { addNote } = context;
-  const [note, setNote] = useState({title:"", description:"",tag:"default"})
+  const [note, setNote] = useState({title:"", description:"",tag:""})
   const handleClick = (e)=>{
       e.preventDefault();
     addNote(note.title, note.description, note.tag);
@@ -40,12 +40,20 @@ const Addnote = () => {
             onChange={onChange}
           />
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
+
+        <div className="mb-3">
+          <label htmlFor="tag" className="form-label">
+            Tag
           </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tag"
+            name="tag"
+            onChange={onChange}
+          />
         </div>
+    
         <button type="submit" className="btn btn-primary" onClick={handleClick}>
           Add Note
         </button>
