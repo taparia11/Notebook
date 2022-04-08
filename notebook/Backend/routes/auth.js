fd13c8bb131649404b6b8687ff57112f5b544760
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-08 21:00:49 +0530

    updated

--- notebook/Backend/routes/auth.js
+++ notebook/Backend/routes/auth.js
@@ -17,14 +17,15 @@ router.post('/createuser',[
 ] , async (req, res)=>{
     // if there are error return bad request and error
     const errors = validationResult(req);
+    success = false;
     if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+        return res.status(400).json({success,errors:errors.array()});
     }
     try{
     //  check whether the user with this email exists already
     let user = await User.findOne({email:req.body.email});
     if (user) {
-        return res.status(400).json({error: "Sorry a user with this email already exists"})
+        return res.status(400).json({success,error: "Sorry a user with this email already exists"})
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -41,8 +42,9 @@ router.post('/createuser',[
             id: user.id
         }
     }
+    success = true;
     const  authtoken = jwt.sign(data, JWT_SECRET);
-    res.json({authtoken})
+    res.json({success,authtoken})
 
     // res.json(user)
 
