964937a4f30e905b3e30f981a440b7b390f3460f
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-08 21:00:55 +0530

    updated

--- notebook/src/App.js
+++ notebook/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,19 +11,32 @@ import About from './components/About'
 import NoteState from "./context/notes/NoteState";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Alert from './components/Alert';
 
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type:type 
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
   return (
     <>
     <NoteState>
     <Router>
       <Navbar title ="FABLAB" />
+      <Alert alert={alert}/>
       <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/" element={<Home showAlert={showAlert} />} />
           <Route exact path="/about" element={<About/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/signup" element={<Signup/>} />
+          <Route exact path="/login" element={<Login showAlert={showAlert}/>}  />
+          <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} />
           {/* <Route exact path="/login" element={<Login />}/> */}
           </Routes>
     </Router>
