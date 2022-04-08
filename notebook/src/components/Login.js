874e813b223d88525062afbd92d2c10f1366c859
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-08 21:01:13 +0530

    updated

--- notebook/src/components/Login.js
+++ notebook/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 
-const Login = () => {
+const Login = (props) => {
 
   const [credentials, setCredentials] = useState({email:"", password:""})
   let navigate = useNavigate();
@@ -20,9 +20,10 @@ const Login = () => {
             //save the auth token and redirect
             localStorage.setItem('token', json.authtoken);
             navigate("/"); 
+            props.showAlert("Logged in successfully","success")
         }
         else{
-            alert('Invalid credentials');
+            props.showAlert("Invalid Credentials","danger")
         }
         
     }
