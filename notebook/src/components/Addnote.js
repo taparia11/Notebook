faff2e5734358dd0489ab8803bab2cd8075f1386
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-08 21:01:00 +0530

    updated

--- notebook/src/components/Addnote.js
+++ notebook/src/components/Addnote.js
@@ -1,13 +1,14 @@
 import React, { useContext, useState } from "react";
 import noteContext from "../context/notes/noteContext";
 
-const Addnote = () => {
+const Addnote = (props) => {
   const context = useContext(noteContext);
   const { addNote } = context;
   const [note, setNote] = useState({title:"", description:"",tag:""})
   const handleClick = (e)=>{
       e.preventDefault();
     addNote(note.title, note.description, note.tag);
+    props.showAlert("Note created successfully","success")
     setNote({title:"", description:"",tag:""})
   }
   const onChange = (e)=>{
